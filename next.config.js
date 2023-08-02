66a6a449bd7d17097d8b70f2a1c2521b159b79d8
paulosilvacliqx <paulo.silva@cliqx.com.br>
2023-08-02 12:17:53 -0300

    fix env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { env:{
-  API_HOST: process.env.X_CSRF_TOKEN,
+  API_HOST: process.env.API_HOST,
   X_CSRF_TOKEN: process.env.X_CSRF_TOKEN,
 }}
 
