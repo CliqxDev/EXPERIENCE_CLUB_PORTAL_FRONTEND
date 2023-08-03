2ab4dc8303d6ec10db4a54894b6dd32b9f7ac8d0
paulosilvacliqx <paulo.silva@cliqx.com.br>
2023-08-03 00:24:53 -0300

    rebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,18 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/global';
 
-import theme from '../styles/theme'
+import theme from '../styles/theme';
 
 export const metadata: Metadata = {
   title: 'EXP Club',
   description: 'EXP Club'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <ThemeProvider theme={theme}>
