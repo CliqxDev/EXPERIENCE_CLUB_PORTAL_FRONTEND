d915d9d70b4bc46b153e1fc85f39c524058c9370
paulosilvacliqx <paulo.silva@cliqx.com.br>
2023-08-03 00:21:52 -0300

    rebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import React from 'react';
-import { Wrapper } from 'styles/styles';
+import GlobalStyles from 'styles/global';
 
-import { NavigationProfile } from 'components/NavigationProfile';
+import Main from 'components/Main';
 
 export default function Home() {
   return (
-    <Wrapper>
-      <NavigationProfile />
-    </Wrapper>
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
   );
 }
